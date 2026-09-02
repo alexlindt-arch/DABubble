d586e6ac31f49b38c1575547ce1bdc19b65c7b4e
ball82 <ballosj@bluewin.ch>
2026-09-02 18:47:46 +0200

    update environment.ts with placeholder Firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyA6u3-0U7KfF1m5OG1vV7UtXMout5G-mtc',
-    authDomain: 'dabubble-93b02.firebaseapp.com',
-    projectId: 'dabubble-93b02',
-    storageBucket: 'dabubble-93b02.firebasestorage.app',
-    messagingSenderId: '68829789989',
-    appId: '1:68829789989:web:b8751b7e88484ff32824cc',
-    measurementId: 'G-8KGCCTP3Y6',
+    apiKey: 'DEINE_API_KEY',
+    authDomain: 'dabubble.firebaseapp.com',
+    projectId: 'dabubble',
+    storageBucket: 'dabubble.appspot.com',
+    messagingSenderId: 'DEINE_SENDER_ID',
+    appId: 'DEINE_APP_ID',
   },
 };
