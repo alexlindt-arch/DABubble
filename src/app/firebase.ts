4c6850d41d8b0a5deae322e07f9020da91a64444
ball82 <ballosj@bluewin.ch>
2026-09-04 09:04:57 +0200

    Fix import path for environment in firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,4 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 
 export const firebaseApp = initializeApp(environment.firebase);
