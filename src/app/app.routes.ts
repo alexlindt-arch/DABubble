1bc79cc9aa4188b7c68deffb6ee961ba1f71c1b2
Julia Schäffer <juliaschaeffer96@gmail.com>
2026-09-07 20:46:36 +0200

    delete forgot password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { ChooseAvatar } from './components/choose-avatar/choose-avatar';
-import { ForgotPassword } from './components/forgot-password/forgot-password';
 import { ResetPassword } from './components/reset-password/reset-password';
 import { SendMail } from './components/send-mail/send-mail';
 import { MainLayout } from './components/main-layout/main-layout';
@@ -27,10 +26,6 @@ export const routes: Routes = [
   {
     path: 'choose-avatar',
     component: ChooseAvatar,
-  },
-  {
-    path: 'forgot-password',
-    component: ForgotPassword,
   },
   {
     path: 'send-mail',
